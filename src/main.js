b7c9fa473436154585cea8eea364f9b64866993b
silvaokindo <okindosilva@gmail.com>
2023-08-11 02:01:30 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
+import "../styles/modern-normalizer.css"
 import "../styles/style.css";
+import "../styles/components/header.css"
+import "../styles/components/hero.css"
+import "../styles/components/services.css"
 import "../styles/utils.css"
